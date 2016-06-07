5a5a8785f37b88dd69fcded3b5a0b8c7983d30e3
Vinicius Amorim de Lima <valima@rederecord.com.br>
2016-06-07 17:29:57 -0300

    Mudança de fonte

--- www/js/custom.js
+++ www/js/custom.js
@@ -1,14 +1,16 @@
 $(document).ready(function(){
-	var margin_body = 0;
-	var height_window = $(window).height();
-   
-	$("#index-banner").height(height_window);
-	$("#index-banner").marginTop(0);
-	
-	$(window).resize(function() {
-		var height_window = $(window).height();
-		$("#index-banner").height(height_window);
-	});
-	
-	
+    var margin_body = 0;
+    var height_window = $(window).height();
+
+    $("#index-banner").height(height_window);
+    $("#index-banner").marginTop(0);
+
+    $("#header-height").marginBottom(height_window - (height_window * 0.6));
+
+    $(window).resize(function() {
+        var height_window = $(window).height();
+        $("#index-banner").height(height_window);
+    });
+
+
 });
